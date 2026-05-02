e006faccf494bd8761ecf287a88cf4c1a9721995
Hassan <hassanisavailable@gmail.com>
2026-05-03 04:42:38 +0500

    style: format UI calendar and picker components

--- components/ui/DateRangePicker.tsx
+++ components/ui/DateRangePicker.tsx
@@ -5,10 +5,10 @@ import { format, isSameDay, isAfter, isBefore } from "date-fns";
 import { Calendar as CalendarIcon, ChevronLeft, ChevronRight } from "lucide-react";
 import { Calendar } from "./Calendar";
 
-export function DateRangePicker({ 
+export function DateRangePicker({
   onRangeChange,
   className = ""
-}: { 
+}: {
   onRangeChange: (start: Date, end: Date) => void,
   className?: string
 }) {
@@ -56,9 +56,9 @@ export function DateRangePicker({
 
       {isOpen && (
         <div className="absolute z-50 mt-2 right-0 animate-in fade-in zoom-in duration-200 origin-top-right">
-          <Calendar 
-            selected={startDate} 
-            onSelect={handleSelect} 
+          <Calendar
+            selected={startDate}
+            onSelect={handleSelect}
           />
           {/* Note: A true range picker would highlight days between, but for simplicity and elegance we'll start with this */}
         </div>
