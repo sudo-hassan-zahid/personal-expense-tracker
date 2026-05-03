18c4672ecf8aa8b04ed997f0f226f5ec64905331
Hassan <hassanisavailable@gmail.com>
2026-05-03 19:08:07 +0500

    mobile-optimization: adjust login page padding and fix accidental text

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -10,7 +10,7 @@ export default async function LoginPage(props: {
   const message = searchParams?.message as string | undefined;
 
   return (
-    <div className="flex-1 flex flex-col bg-(--color-canvas-dark) text-(--color-on-dark) justify-center py-12 sm:px-6 lg:px-8 relative overflow-hidden min-h-screen">
+    <div className="flex-1 flex flex-col bg-(--color-canvas-dark) text-(--color-on-dark) justify-center py-6 md:py-12 px-4 sm:px-6 lg:px-8 relative overflow-hidden min-h-screen">
       {/* Background glow effects */}
       <div className="absolute top-[-10%] left-[-10%] w-96 h-96 bg-blue-500/20 rounded-full blur-[120px] pointer-events-none" />
       <div className="absolute bottom-[-10%] right-[-10%] w-96 h-96 bg-purple-500/20 rounded-full blur-[120px] pointer-events-none" />
