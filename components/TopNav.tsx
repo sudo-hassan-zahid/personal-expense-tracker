6091ad52d0db168ae63487ba5a68cfdf5f5a8461
Hassan <hassanisavailable@gmail.com>
2026-05-04 01:03:37 +0500

    Avoid anonymous nav auth lookup

--- components/TopNav.tsx
+++ components/TopNav.tsx
@@ -2,10 +2,21 @@
  * Component: TopNav.tsx
  */
 import { createClient } from "@/lib/supabase";
+import { cookies } from "next/headers";
 import { TopNavClient } from "./TopNavClient";
 
+function hasSupabaseAuthCookie(allCookies: { name: string }[]) {
+  return allCookies.some((cookie) => cookie.name.startsWith("sb-"));
+}
+
 export async function TopNav({ activeTheme }: { activeTheme: "light" | "dark" }) {
-  const supabase = await createClient();
+  const cookieStore = await cookies();
+  const allCookies = cookieStore.getAll();
+  if (!hasSupabaseAuthCookie(allCookies)) {
+    return <TopNavClient user={null} activeTheme={activeTheme} />;
+  }
+
+  const supabase = await createClient(allCookies);
   const { data } = await supabase.auth.getUser();
   const user = data?.user;
 
