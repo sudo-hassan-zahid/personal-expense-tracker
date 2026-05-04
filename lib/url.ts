535f3fc3db959457665b04e89b9979cfb727f7d0
Hassan <hassanisavailable@gmail.com>
2026-05-04 15:47:24 +0500

    chore: formatted 'lib/url.ts'

--- lib/url.ts
+++ lib/url.ts
@@ -7,7 +7,7 @@ export function getSiteUrl() {
     process.env.NEXT_PUBLIC_SITE_URL ||
     (process.env.NEXT_PUBLIC_VERCEL_URL ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` : "") ||
     "http://localhost:3000";
-  
+
   // Remove trailing slash if present
   return url.replace(/\/$/, "");
 }
